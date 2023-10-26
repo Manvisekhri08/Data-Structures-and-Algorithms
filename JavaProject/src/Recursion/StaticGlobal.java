f9aeb6be8b42c512cbc32e505d3e8d86814f0101
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-10-26 23:36:25 +0530

    calling the global var thrice

--- JavaProject/src/Recursion/StaticGlobal.java
+++ JavaProject/src/Recursion/StaticGlobal.java
@@ -1,22 +1,33 @@
 package Recursion;
 
 public class StaticGlobal {
-    static int y = 0; // Declare y as a static class member
-
+   // static int y = 0; // Declare y as a static class member
+    int z = 0;
     int funGlobal(int n) {
         if (n > 0) {
-            y++;
-            return funGlobal(n - 1) + y;
+            z++;
+            return funGlobal(n - 1) + z;
         }
         return 0;
     }
 
     public static void main(String[] args) {
-        int r = 5;
+        int r;
+        r = 5;
         StaticGlobal global = new StaticGlobal();
-        System.out.print(global.funGlobal(r));
+        System.out.println(global.funGlobal(r));
+        r = 5;
+        System.out.println(global.funGlobal(r));
+        r = 5;
+        System.out.println(global.funGlobal(r));
     }
 }
 
 //with static = 25
 // with global = 15
+// with global z = 25
+
+// with calling it twice o/p :- 25 & 50
+// when first call is made the value of x is 5
+// when second call is made again trying to increment x, for 5 times here x++, then x, will become 10
+// when third time x is 10 and it increment to 15;
