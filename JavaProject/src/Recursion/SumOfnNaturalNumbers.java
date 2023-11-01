0e76bac187f5b853c32f171856cf86b2ef0a0959
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-11-01 12:08:58 +0530

    Sum of n natural numbers

---
+++ JavaProject/src/Recursion/SumOfnNaturalNumbers.java
@@ -0,0 +1,37 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfnNaturalNumbers {
+    
+    // USING RECURSION
+    // int Rsum(int n) {
+    //     if(n == 0) {
+    //         return 0;
+    //     } else {
+    //         return sum(n-1)+n;
+    //     }
+    // }
+
+    // USING LOOP
+    // int Isum(int n) {
+    //     int i, s =0;
+    //     for(i=1 ; i<=n ; i++) {
+    //         s=s+i;
+    //     }
+    //     return s;
+    // }
+
+    //USING FORMULA
+    int Fsum(int n) {
+        return (n*(n+1))/2;
+    }
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  
+        System.out.println("Enter number");
+        Integer userNumber = myObj.nextInt();
+        SumOfnNaturalNumbers rec = new SumOfnNaturalNumbers();
+        System.out.println(rec.Fsum(userNumber));
+    }
+}
