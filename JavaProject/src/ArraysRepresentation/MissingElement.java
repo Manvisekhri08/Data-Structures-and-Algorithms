013582c39ebc7b25bba6b31ba6b86adf302f8db7
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-11-27 21:32:06 +0530

    missing element using hash tables

--- JavaProject/src/ArraysRepresentation/MissingElement.java
+++ JavaProject/src/ArraysRepresentation/MissingElement.java
@@ -1,21 +1,35 @@
 package ArraysRepresentation;
 
 import java.util.Scanner;
+import java.util.HashSet;
 
 public class MissingElement {
     void missing(int[] A) {
-        int low = A[0];
-        int diff = low - 0;
-        for(int i =0; i<A.length;i++) {
-            // single missing element
-            //  if(A[i] - i != diff) {
-            //     System.out.println("Missing Element is found which is:" +( diff + i));
-            //     break;
-            // }
-            // multiple missing elements
-            while(A[i] - i != diff) {
-                System.out.println("Missing Element is found which is:" +( diff + i));
-                diff++;
+        // int low = A[0];
+        // int diff = low - 0;
+        // for(int i =0; i<A.length;i++) {
+        //     // single missing element
+        //     //  if(A[i] - i != diff) {
+        //     //     System.out.println("Missing Element is found which is:" +( diff + i));
+        //     //     break;
+        //     // }
+        //     // multiple missing elements
+        //     while(A[i] - i != diff) {
+        //         System.out.println("Missing Element is found which is:" +( diff + i));
+        //         diff++;
+        //     }
+        // }
+        // using hashing/hash tables
+        HashSet<Integer> hashSet = new HashSet<>();
+        // Add all elements from the array to the HashSet
+        for(int num : A) {
+            hashSet.add(num);
+        }
+        System.out.print("Missing Elements are: ");
+        // Check for missing elements from 1 to the maximum element in the array
+        for(int i = 1; i <= A.length + 1; i++) {
+            if(!hashSet.contains(i)) {
+                System.out.println(i);
             }
         }
     }
