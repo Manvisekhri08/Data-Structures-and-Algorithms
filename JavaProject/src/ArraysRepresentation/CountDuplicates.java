26ec8b7afeb5d351f896d2f8945cfc33fd54aaeb
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-11-28 19:16:15 +0530

    counting duplicates using hashset

--- JavaProject/src/ArraysRepresentation/CountDuplicates.java
+++ JavaProject/src/ArraysRepresentation/CountDuplicates.java
@@ -1,19 +1,39 @@
 package ArraysRepresentation;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class CountDuplicates {
     void countDup(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-            int j;
-            if (i< A.length && A[i] == A[i + 1]) {
-                j = i + 1;
-                while (j < A.length &&A[i] == A[j]) {
-                    j++;
-                }
-                System.out.println("Duplicate elements are: " + A[i] + " Frequency is: " + (j - i));
-                i = j - 1;
+        // for (int i = 0; i < A.length; i++) {
+        //     int j;
+        //     if (i< A.length && A[i] == A[i + 1]) {
+        //         j = i + 1;
+        //         while (j < A.length &&A[i] == A[j]) {
+        //             j++;
+        //         }
+        //         System.out.println("Duplicate elements are: " + A[i] + " Frequency is: " + (j - i));
+        //         i = j - 1;
+        //     }
+        // }
+        // using hashset
+        HashSet<Integer> unique = new HashSet<>();
+        HashSet<Integer> duplicate = new HashSet<>();
+        for(int i = 0; i<A.length;i++) {
+            if(!unique.add(A[i])) {
+                // Element is already present in the set, meaning it's a duplicate
+                duplicate.add(A[i]);
             }
+        }
+        // Iterate through duplicate elements and print their frequencies
+        for(int element : duplicate) {
+            int frequency = 0 ;
+            for(int num : A) {
+                if(element == num) {
+                    frequency++;
+                }
+            }
+            System.out.println("Duplicate element: " + element + " Frequency is: " + frequency);
         }
     }
 
