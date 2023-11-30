664848225262b1b4b72d0efeb80f261554f2f18a
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-11-30 13:55:06 +0530

    pair of elements with sum K using hashset

--- JavaProject/src/ArraysRepresentation/PairSumK.java
+++ JavaProject/src/ArraysRepresentation/PairSumK.java
@@ -1,5 +1,6 @@
 package ArraysRepresentation;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class PairSumK {
@@ -11,6 +12,16 @@ public class PairSumK {
                     System.out.println("Pair of sum equal to "+ k + "are: " + "{ " + A[i] + "," + A[j] + " }" );
                 }
             }
+        }
+    }
+    // using hashing
+    void pairSumKHashing(int[] A, int k) {
+        HashSet<Integer> pair = new HashSet<>();
+        for(int num: A) {
+            if(pair.contains(k-num)) {
+                System.out.println("Pair of sum equal to "+ k + " are: " + "{ " + (k-num) + "," + num + " }" );
+            }
+            pair.add(num);
         }
     }
     public static void main(String[] args) {
@@ -26,7 +37,7 @@ public class PairSumK {
         System.out.print("Enter the sum: ");
         k = scanner.nextInt();
         PairSumK sumK = new PairSumK();
-        sumK.pairSum(A, k);
+        sumK.pairSumKHashing(A, k);
         scanner.close();
     }
 }
