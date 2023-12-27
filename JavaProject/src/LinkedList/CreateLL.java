cacfc1fd5483a94240706edb2cfea5cec8b262f5
Manvi Sekhri <ManviSekhri08@gmail.com>
2023-12-27 23:53:33 +0530

    Create Linked List

---
+++ JavaProject/src/LinkedList/CreateLL.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class CreateLL {
+    public static void main(String args[]) {
+        Node p1 = new Node(10);
+        Node p2 = new Node(20);
+        Node p3 = new Node(30);
+        Node p4 = new Node(40);
+        p1.next = p2;
+        p2.next = p3;
+        p3.next = p4;
+        Node head = p1;
+        Node curr = head;
+        while(curr != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+    }
+}
