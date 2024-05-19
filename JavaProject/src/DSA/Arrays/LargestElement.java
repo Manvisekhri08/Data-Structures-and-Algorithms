b832ab2f251c81f8a0c344acb6136c5c51a90a15
ManviSekhri08 <manvisekhri08@gmail.com>
2024-05-19 13:49:50 +0530

    Largest Element in array

--- JavaProject/src/DSA/Arrays/LargestElement.java
+++ JavaProject/src/DSA/Arrays/LargestElement.java
@@ -24,32 +24,34 @@ package DSA.Arrays;
 import java.util.*;
 import java.util.Scanner;
 
-
 public class LargestElement {
     public void largest(int arr[]) {
         /*
          * Brute
          * sort the array
          * the last index is our answer
+         * TC :- O(NlogN)
          */
         Arrays.sort(arr);
-        System.out.println("Largest element in array is : "+ arr[arr.length-1]);
+        System.out.println("Largest element in array is : " + arr[arr.length - 1]);
 
         /*
          * Optimal
          * we will assume 1st lement is the largest
          * now we traverse through array and compare with largest
          * if current elemnt is greater then we update the largest
+         * TC:- O(N)
          */
         int largest = arr[0];
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] > largest) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
                 largest = arr[i];
             }
         }
-        System.out.println("Largest element in array is : "+ largest);
+        System.out.println("Largest element in array is : " + largest);
     }
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter length of array : ");
         int size = scanner.nextInt();
